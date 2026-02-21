38bd920d7a3e2c17101315896e2e533e49627a03
Hasan <arnobhasanice@gmail.com>
2026-02-21 22:25:25 +0600

    2

--- bin/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:dart_simple_project/greet.dart';
-
-void main(List<String> arguments) {
-  final message = greet(arguments.isNotEmpty ? arguments.first : 'World');
-  print(message);
-}
--- test/greet_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:test/test.dart';
-import 'package:dart_simple_project/greet.dart';
-
-void main() {
-  test('greet returns a friendly message', () {
-    expect(greet('World'), equals('Hello, World!'));
-  });
-}
