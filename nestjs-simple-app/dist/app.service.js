1c3162860ccb3aaf8c86af498f25dd3500f965d1
Hasan <arnobhasanice@gmail.com>
2026-02-22 14:55:47 +0600

    7

--- nestjs-simple-app/dist/app.service.js
+++ nestjs-simple-app/dist/app.service.js
@@ -5,16 +5,44 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.AppService = void 0;
 const common_1 = require("@nestjs/common");
+const typeorm_1 = require("@nestjs/typeorm");
+const typeorm_2 = require("typeorm");
+const app_entity_1 = require("./app.entity");
 let AppService = class AppService {
+    constructor(todoRepository) {
+        this.todoRepository = todoRepository;
+    }
     getHello() {
-        return 'Hello World!';
+        return 'Hello from NestJS backend!';
+    }
+    async getAllTodos() {
+        return this.todoRepository.find();
+    }
+    async createTodo(title) {
+        const todo = this.todoRepository.create({ title });
+        return this.todoRepository.save(todo);
+    }
+    async updateTodo(id, completed) {
+        await this.todoRepository.update(id, { completed });
+        return this.todoRepository.findOne({ where: { id } });
+    }
+    async deleteTodo(id) {
+        await this.todoRepository.delete(id);
     }
 };
 exports.AppService = AppService;
 exports.AppService = AppService = __decorate([
-    (0, common_1.Injectable)()
+    (0, common_1.Injectable)(),
+    __param(0, (0, typeorm_1.InjectRepository)(app_entity_1.Todo)),
+    __metadata("design:paramtypes", [typeorm_2.Repository])
 ], AppService);
 //# sourceMappingURL=app.service.js.map
