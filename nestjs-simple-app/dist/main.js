0b20bff788a4d9ec9c861a72372d53a291c13fb5
Hasan <arnobhasanice@gmail.com>
2026-02-22 12:33:25 +0600

    6

--- nestjs-simple-app/dist/main.js
+++ nestjs-simple-app/dist/main.js
@@ -3,10 +3,16 @@ Object.defineProperty(exports, "__esModule", { value: true });
 require("reflect-metadata");
 const core_1 = require("@nestjs/core");
 const platform_fastify_1 = require("@nestjs/platform-fastify");
+const path_1 = require("path");
 const app_module_1 = require("./app.module");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule, new platform_fastify_1.FastifyAdapter());
+    app.useStaticAssets({
+        root: (0, path_1.join)(__dirname, '..', 'public'),
+        prefix: '/',
+    });
     await app.listen(3000);
+    console.log('Server running on http://localhost:3000');
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
--- nestjs-simple-app/src/app.service.ts
+++ nestjs-simple-app/src/app.service.ts
@@ -1,8 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Todo } from './app.entity';
 
 @Injectable()
 export class AppService {
+  constructor(
+    @InjectRepository(Todo)
+    private todoRepository: Repository<Todo>,
+  ) {}
+
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello from NestJS backend!';
+  }
+
+  async getAllTodos(): Promise<Todo[]> {
+    return this.todoRepository.find();
+  }
+
+  async createTodo(title: string): Promise<Todo> {
+    const todo = this.todoRepository.create({ title });
+    return this.todoRepository.save(todo);
+  }
+
+  async updateTodo(id: number, completed: boolean): Promise<Todo> {
+    await this.todoRepository.update(id, { completed });
+    return this.todoRepository.findOne({ where: { id } });
+  }
+
+  async deleteTodo(id: number): Promise<void> {
+    await this.todoRepository.delete(id);
   }
 }
